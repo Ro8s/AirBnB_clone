2b48ca89fd6a72e95d1a1cafb18ded7d89c8b2b7
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-24 19:27:28 +0000

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,26 @@ import uuid
 class BaseModel():
     ''' BaseModel class '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         ''' init method '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+        else:
+            for key in kwargs:
+                if key is "created_at" or key is "updated_at":
+                    a = datetime.strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, a)
+                elif key is not "__class__":
+                    setattr(self, key, kwargs[key])
         self.updated_at = datetime.now()
 
     ''' __str__ '''
 
     def __str__(self):
         ''' __str__ '''
-        return "[{}] ({}) {}".format(self.__class__.__name__ , self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     ''' save mehtod '''
 
