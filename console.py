268b19c936d923f6fad8d22e567370f5e2b48a61
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-28 13:00:00 +0000

    Curriculm Vitae do madam Carai

--- console.py
+++ console.py
@@ -2,7 +2,9 @@
 ''' The console '''
 
 import cmd
-
+from models.base_model import BaseModel
+from sys import argv
+from shlex import split
 
 class HBNBCommand(cmd.Cmd):
     ''' El console '''
@@ -20,8 +22,29 @@ class HBNBCommand(cmd.Cmd):
         ''' exit console '''
         return True
 
+    ''' Create '''
 
+    def do_create(self, line):
+        '''  Creates a new instance of BaseModel '''
+        s = split(line)
+        if len(s) < 1:
+            print("** class name missing **")
+        elif s[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            insta = BaseModel()
+            insta.save()
+            print(insta.id)
 
+    ''' Show '''
+    def do_show(self, line):
+        ''' Prints the string representation of an instance based on the class name and id '''
+        
+    ''' Empty line '''
+
+    def emptyline(self):
+        ''' empty line '''
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
