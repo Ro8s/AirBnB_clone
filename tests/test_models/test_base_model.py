b5c670a828f48a8c5c958c7ac731e159aedfb505
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-29 23:08:35 +0000

    avergaston

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,22 +4,29 @@
 import models
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
     ''' Test for BaseModel '''
 
-    def TestBaseModelex3(self):
+    def test_basemodel(self):
         '''  Test for BaseModel ''' 
         b1 = BaseModel()
         self.assertIn(b1, models.storage.all().values())
-
-        ''' DIEGOTE '''
-    def TestBaseModelex31(self):
-        ''' Test for BaseModel '''
-        b1 = BaseModel() 
         self.assertEqual(datetime, type(b1.created_at))
+        self.assertEqual(datetime, type(b1.updated_at))
+        self.assertEqual(dict, type(b1.to_dict()))
 
+    ''' Test for BaseModel '''
+
+    def test_basemodels(self):
+        ''' Test for 2 classes BaseModel '''
+        b1 = BaseModel()
+        b2 = BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
+        self.assertNotEqual(b1.created_at, b2.created_at)
+        self.assertNotEqual(b1.created_at, b2.created_at)
 
 if __name__ == '__main__':
     unittest.main()
