83c59f4a82d58c4ffc103e510cdaa1cf9160a742
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-30 21:06:08 +0000

    15

--- console.py
+++ console.py
@@ -247,13 +247,19 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             return
         if s[1][0:7] == 'update(' and s[1][-1] == ')':
-            if s[1][-2] == '}': #Caso id, {diccionario}
-                's[1] = update("234", {"name": "kiko", "age": 20})'
+            if s[1][-2] == '}': #Caso id, {diccionario} (ej 16)
                 t = s[1].split('"', 2)
                 dic = eval(t[2][2:-1])
                 for key, values in dic.items():
                     parse = s[0] + " " + t[1] + " " + str(key) + " " + str(values)
                     self.do_update(parse)
+                    return
+            else: #Corte ejercicio 15 sacas
+                's[1] = update("234", "name", "kiko")'
+                h = s[1].split('"')
+                linea = s[0] + " " + h[1] + " " + str(h[3]) + " " + str(h[5])
+                self.do_update(linea)
+                return
 
     ''' Empty line '''
 
